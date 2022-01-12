5bb0c59b3df0646fb46019354a23fa67e0680ba1
LibrarristShalinward <1701427793@qq.com>
2022-01-12 16:21:38 +0800

    添加了README

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from snow_fall import au2snow
 
 if __name__ == "__main__": 
     parser = argparse.ArgumentParser("落雪特效")
-    parser.add_argument("-f", "--file", help = "将.wav文件转化为落雪视频（.avi格式），或将中间文件.awrp或.sfev转化为视频")
+    parser.add_argument("file", help = "将.wav文件转化为落雪视频（.avi格式），或将中间文件.awrp或.sfev转化为视频")
     args = parser.parse_args()
     au2snow(args.file)
